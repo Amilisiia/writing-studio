b84f845af0d80cc6fc07673c371c1cdddd2a885e
Анастасія <anastasiia55@gmail.com>
2026-01-07 14:34:19 +0200

    feat: setup Firebase integration
    - Add firebase.config.js with project configuration
    - Install Firebase SDK and dependencies
    - Enable Authentication (Email/Password)
    - Create Firestore Database
    - Setup Firestore Security Rules

---
+++ public/js/config/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNL9nf68As0uyuCnDs-eUsczIkBzZT6F8",
+    authDomain: "writing-studio-5a7fc.firebaseapp.com",
+    projectId: "writing-studio-5a7fc",
+    storageBucket: "writing-studio-5a7fc.firebasestorage.app",
+    messagingSenderId: "759410399496",
+    appId: "1:759410399496:web:715b5abe638d201a4b34bd"
+};
+
+export default firebaseConfig;
